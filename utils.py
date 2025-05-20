d8a55a0d453139f7d757c302eed55b8755b3ceb7
olesiaBokhonok <olesia.bokhonok.ki.2022@lpnu.ua>
2025-05-20 23:50:15 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,22 +1,10 @@
-import logging
-from datetime import datetime
-import winreg
 import socket
-import sys
+import winreg
+from datetime import datetime
 from plyer import notification
 
-# Налаштування змінної середовища для Tkinter
-import os
-os.environ["DISPLAY"] = ":0"
-
-# Налаштування логування
-logging.basicConfig(filename='backup.log', level=logging.INFO,
-                    format='%(asctime)s - %(levelname)s - %(message)s', encoding='utf-8')
-
-def log_operation(message):
-    logging.info(message)
-
 def notify_backup_completed(backup_type, filepath, last_notify=[None]):
+    """Відправляє сповіщення про успішне копіювання."""
     current_time = datetime.now()
     if not last_notify[0] or (current_time - last_notify[0]).total_seconds() > 60:
         notification.notify(
@@ -28,6 +16,7 @@ def notify_backup_completed(backup_type, filepath, last_notify=[None]):
         last_notify[0] = current_time
 
 def notify_error(message):
+    """Відправляє сповіщення про помилку."""
     notification.notify(
         title="Помилка резервного копіювання",
         message=message,
@@ -35,19 +24,8 @@ def notify_error(message):
         timeout=5
     )
 
-def add_to_startup():
-    try:
-        script_path = os.path.abspath(sys.argv[0])
-        key = winreg.OpenKey(winreg.HKEY_CURRENT_USER,
-                             r"Software\Microsoft\Windows\CurrentVersion\Run",
-                             0, winreg.KEY_SET_VALUE)
-        winreg.SetValueEx(key, "BackupApp", 0, winreg.REG_SZ, f'"{sys.executable}" "{script_path}" -startMinimized')
-        winreg.CloseKey(key)
-        logging.info("Програма додана до автозапуску.")
-    except Exception as e:
-        logging.error(f"Помилка додавання до автозапуску: {e}")
-
 def is_running():
+    """Перевіряє, чи програма вже запущена."""
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.bind(("localhost", 12345))
@@ -55,3 +33,13 @@ def is_running():
         return False
     except socket.error:
         return True
+
+def add_to_startup():
+    """Додає програму до автозавантаження Windows."""
+    try:
+        key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"Software\Microsoft\Windows\CurrentVersion\Run", 0, winreg.KEY_SET_VALUE)
+        winreg.SetValueEx(key, "BackupApp", 0, winreg.REG_SZ, f'"{sys.executable}" "{os.path.abspath(__file__)}" -startMinimized')
+        winreg.CloseKey(key)
+        logging.info("Програму додано до автозавантаження")
+    except Exception as e:
+        logging.error(f"Помилка додавання до автозавантаження: {e}")
